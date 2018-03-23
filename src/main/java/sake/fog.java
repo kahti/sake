914cd261a22dc008d5927857a157a17d73df3957
ranjith <ranjithkumar143341@gmail.com>
2018-03-23 10:19:32 +0530

    this the first

---
+++ src/main/java/sake/fog.java
@@ -0,0 +1,7 @@
+package sake;
+
+public class fog {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/sake/dog.java
@@ -0,0 +1,14 @@
+package sake;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class dog {
+
+	@Test
+	public void test() {
+	
+	}
+
+}
